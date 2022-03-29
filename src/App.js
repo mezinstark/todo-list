fb0bf7647bcc840cabafc781e546f3659e4c7e9f
Mezin Stark <54954471+mezinstark@users.noreply.github.com>
2022-03-29 07:20:44 +0530

    final Commit for: task Tracker(todo-list)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { TextField } from './components/TextField/textField.components';
+
 
 function App() {
+
+  const [todos, setTodos] =useState([]);
+  const [check, setCheck] =useState([]);
+  
+
+  const addItem=(event)=>{
+   event.preventDefault();
+    const itemValue =event.target.item.value;
+    if(itemValue!==""){
+    setTodos(values=>[...values, itemValue]);
+    setCheck(values=>[...values, false]);
+    }
+    else{
+      alert(`PLease Type a task before Submiting`);
+    }
+  }
+
+  const handleChange=(position)=>{
+    const updatedChecked=check.map((item, index)=>
+        index === position? !item :item
+        );
+        setCheck(updatedChecked);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <div className='todo-container'>
+     <h1>TASK Tracker!</h1>
+    <TextField 
+    item={todos}
+    checkItem={check}
+    addItem={addItem}
+    handleChange={handleChange}
+    />
+   </div>
+   <div>
+     
+   </div>
+   </>
   );
 }
 
